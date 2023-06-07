acc1734cf7d81a51cc68aa1879453f2ff565b930
Flávio Farias <Fs.flaviosilv4@gmail.com>
2023-06-07 18:25:12 -0300

    Commit Interface

---
+++ Assets/Scripts/Configuracoes/ResolutionDropdown.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ResolutionDropdown : MonoBehaviour
+{
+    public TMP_Dropdown resolutionDropdown;
+
+    private Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex) {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void Set4KResolution()
+    {
+        SetResolution(0); // Assuming 4K resolution is the first option in the dropdown
+    }
+
+    public void SetQuadHDResolution()
+    {
+        SetResolution(1); // Assuming QuadHD resolution is the second option in the dropdown
+    }
+
+    public void SetFullHDResolution()
+    {
+        SetResolution(2); // Assuming Full HD resolution is the third option in the dropdown
+    }
+
+    public void SetHDResolution()
+    {
+        SetResolution(3); // Assuming HD resolution is the fourth option in the dropdown
+    }
+}
---
+++ Assets/Scripts/Configuracoes/TelaCheia.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TelaCheia : MonoBehaviour {
+
+    public void SetFullscreen(bool a) {
+        Screen.fullScreen = a;
+    }
+
+}
